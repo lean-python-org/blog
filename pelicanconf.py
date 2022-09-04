20db174154b48c85730c7efa4952ef9691d4843f
Ben Denham <ben@denham.nz>
2022-09-04 13:42:27 +1200

    Fix home page subtitle

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,11 +1,11 @@
 AUTHOR = 'Grant Paton-Simpson & Ben Denham'
 SITENAME = 'The When of Python Blog'
-DESCRIPTION = '''
-<div class="text-muted" style="padding-bottom: 10px;">Guidance on when to use Python features (and when not to)</div>
+SITESUBTITLE = 'Guidance on when to use Python features (and when not to)'
+SITEDESCRIPTION = '''
+<div class="text-muted" style="padding-bottom: 10px;">Guidance on when to use Python features (and when not to).</div>
 <div class="text-muted" style="padding-bottom: 10px;">Comment your opinions on our articles to join the discussion.</div>
 <div class="text-muted" style="padding-bottom: 10px;">Follow us on <a href="https://twitter.com/WhenOfPython">Twitter</a> and <a href="https://github.com/when-of-python">GitHub</a> to get the latest updates first.</div>
 '''
-SITESUBTITLE = DESCRIPTION
 SITEIMAGE = 'images/logo.png'
 SITEURL = ''
 
